ed1369d5cdb4b9418baf781b5e0345023c680b94
Praveeja-Sasidharan-Suni <pravi.sarajit@gmail.comgit config --global user.name Praveeja-Sasidharan-Sunigit config --global user.email pravi.sarajit@gmail.com>
2021-07-03 11:44:33 -0500

    Adding pypoll.py to the repository

---
+++ pypoll.py
@@ -0,0 +1,26 @@
+#Importing the dependencies and modules
+
+import os
+import csv
+#The data we need to retrieve
+file_to_load = os.path.join("Resources", "election_results.csv")
+#Open the election results and read the file.
+with open(file_to_load) as election_data:
+    # Print the file object.
+    print(election_data)
+    #Creating a text file to save the results
+    file_to_save=os.path.join("analysis","Election_Analysis.txt")
+    #open(file_to_save, "w")
+    with open(file_to_save, "w") as txt_file:
+        # Write three counties to the file.
+        txt_file.write("Counties in the Election\n")
+        txt_file.write("--------------------------------\n")
+        txt_file.write("Arapahoe\nDenver\nJefferson")
+   
+        #Read and analyse the data here
+        # Read the file object with the reader function.
+        file_reader = csv.reader(election_data)
+         # Print the header row.
+        headers = next(file_reader)
+        print(headers)
+
