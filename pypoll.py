d09b91c034ed711656fb1eb0604a2fd00690ed7a
Praveeja-Sasidharan-Suni <pravi.sarajit@gmail.com>
2021-07-03 14:52:59 -0500

    adding paypoll.py

--- pypoll.py
+++ pypoll.py
@@ -8,6 +8,9 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 with open(file_to_load) as election_data:
     # Print the file object.
     print(election_data)
+    # 1. Initialize a total vote counter.
+    total_votes = 0
+
     #Creating a text file to save the results
     file_to_save=os.path.join("analysis","Election_Analysis.txt")
     #open(file_to_save, "w")
@@ -23,4 +26,12 @@ with open(file_to_load) as election_data:
          # Print the header row.
         headers = next(file_reader)
         print(headers)
+        # Print each row in the CSV file.
+        for row in file_reader:
+             #  Add to the total vote count.
+                total_votes += 1
+
+        # 3. Print the total votes.
+        print(total_votes)
+
 
